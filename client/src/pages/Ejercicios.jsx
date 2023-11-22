7fc6bc410b94d75bdfe87c1f9bf668fa5b208957
Marta <martagranado1011@uma.es>
2023-11-22 10:31:54 +0100

    Confirmación de meter cliente

--- client/src/pages/Ejercicios.jsx
+++ client/src/pages/Ejercicios.jsx
@@ -10,12 +10,21 @@ const listaDeEjercicios = [
 
 export function Ejercicios() {
   const [mostrarVentana, setMostrarVentana] = useState(false);
+  const [mostrarVentanaConfirmacion, setMostrarVentanaConfirmacion] = useState(false);
 
   const abrirVentana = () => {
     setMostrarVentana(true);
   };
 
   const cerrarVentana = () => {
+    setMostrarVentana(false);
+  };
+  const abrirVentanaConfirmacion = () => {
+    setMostrarVentanaConfirmacion(true);
+  };
+
+  const cerrarVentanaConfirmacion = () => {
+    setMostrarVentanaConfirmacion(false);
     setMostrarVentana(false);
   };
 
@@ -85,11 +94,24 @@ export function Ejercicios() {
            
             <label><input type= "checkbox" value = "Privado" /> Privado </label>
             
-            <button onClick={cerrarVentana}>Confirmar</button>
+            <button onClick={abrirVentanaConfirmacion}>Confirmar</button>
             {/* Este debería mostrar una ventana de ejercicio añadido y mostrarlo */}
             </div>
           </div>
         </div>
+      )}
+
+      {mostrarVentanaConfirmacion && (
+         <div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', background: 'rgba(0, 0, 0, 0.5)', zIndex: '1' }}>
+         <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: 'white', padding: '20px', borderRadius: '8px', width: '300px' }}>
+           
+           <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+           <h4>El ejercicio ha sido añadido</h4>
+           <button onClick={cerrarVentanaConfirmacion}>Ok</button>
+           {/* Cierra la ventana y te da mensaje positivo pero no añade nada aún!!!! */}
+           </div>
+           </div>
+          </div> 
       )}
     </div>
   );
