ffbfb393ef6207a2ab4fe74e8fdfec157da0e82f
BrotherZ11 <dzarzavillaborrego@uma.es>
2023-11-22 11:08:26 +0100

    Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,27 @@
+import React from 'react';
+import '../css/Home.css';
+
 export function Home() {
   return (
-    <div>
-      <h2>FITNESS WELL-BEING</h2>
-      <nav style={{ textAlign: 'center', marginTop: '20px', backgroundColor: "#bec8d1"}}>
-        <div style={{paddingBottom:'20px'}}>
-       <button> <a href="/ejercicios">Ejercicios</a></button>
-       {/* RUTA A EJERCICIO ^ */}
-       </div>
-       <div style={{paddingBottom:'20px'}}>
-       <button> <a href="/rutinas">Rutinas</a> </button>
-       {/* RUTA A RUTINAS ^ */}
-       </div>
-       <div>
-       <button> <a href="/sesiones">Sesiones</a> </button>
-       {/* RUTA A SESIONES ^ */}
-       </div>
+    <div className="home-container">
+      <h2 className="home-title">FITNESS WELL-BEING</h2>
+      <nav className="home-nav">
+        <div>
+          <button className="home-button">
+            <a href="/ejercicios">Ejercicios</a>
+          </button>
+        </div>
+        <div>
+          <button className="home-button">
+            <a href="/rutinas">Rutinas</a>
+          </button>
+        </div>
+        <div>
+          <button className="home-button">
+            <a href="/sesiones">Sesiones</a>
+          </button>
+        </div>
       </nav>
     </div>
-  )
+  );
 }
