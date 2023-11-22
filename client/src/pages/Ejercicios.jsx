077679071c066a0992e8e09b8b078084b2599ec2
Marta <martagranado1011@uma.es>
2023-11-22 12:54:56 +0100

    Añadidas interacciones con los botones
    Interacciones con los botones Ver Modificar y Añadir

--- client/src/pages/Ejercicios.jsx
+++ client/src/pages/Ejercicios.jsx
@@ -11,6 +11,11 @@ const listaDeEjercicios = [
 export function Ejercicios() {
   const [mostrarVentana, setMostrarVentana] = useState(false);
   const [mostrarVentanaConfirmacion, setMostrarVentanaConfirmacion] = useState(false);
+  const [mostrarInfo, setMostrarInfo] = useState(false);
+  const [modificarInfo, setModificarInfo] = useState(false);
+  const [mostrarVentanaConfirmacionModif, setMostrarVentanaConfirmacionModif] = useState(false);
+  const [preguntaBorrado, setPreguntaBorrado] = useState(false);
+  const [mostrarVentanaConfirmacionBorrado, setMostrarVentanaConfirmacionBorrado] = useState(false);
 
   const abrirVentana = () => {
     setMostrarVentana(true);
@@ -26,6 +31,45 @@ export function Ejercicios() {
   const cerrarVentanaConfirmacion = () => {
     setMostrarVentanaConfirmacion(false);
     setMostrarVentana(false);
+  };
+
+  const abrirInfo = () => {
+    setMostrarInfo(true);
+  };
+  const cerrarInfo = () => {
+    setMostrarInfo(false);
+  };
+
+  const modifAbrirInfo = () => {
+    setModificarInfo(true);
+  };
+  const modifCerrarInfo = () => {
+    setModificarInfo(false);
+  };
+
+  const abrirVentanaConfirmacionModif = () => {
+    setMostrarVentanaConfirmacionModif(true);
+  };
+
+  const cerrarVentanaConfirmacionModif = () => {
+    setMostrarVentanaConfirmacionModif(false);
+    setModificarInfo(false);
+  };
+
+  const abrePreguntaBorrado = () => {
+    setPreguntaBorrado(true);
+  };
+  const cierraPreguntaBorrado = () => {
+    setPreguntaBorrado(false);
+  };
+
+  const abrirVentanaConfirmacionBorrado = () => {
+    setMostrarVentanaConfirmacionBorrado(true);
+  };
+
+  const cerrarVentanaConfirmacionBorrado = () => {
+    setMostrarVentanaConfirmacionBorrado(false);
+    setPreguntaBorrado(false);
   };
 
 {/* WIP: VENTANA DE CONFIRMACION DE AÑADIR EJERCICIO (Ejercicio añadido)*/}
@@ -54,9 +98,9 @@ export function Ejercicios() {
                 {ejercicio.nombre} | {ejercicio.tipo} | {ejercicio.privado ? 'Privado' : 'Público'}
               </div>
               <div style={{ display: 'inline-block' }}>
-                <button>Ver</button>
-                <button>Modificar</button>
-                <button>Borrar</button>
+                <button onClick={abrirInfo}>Ver</button>
+                <button onClick={modifAbrirInfo}>Modificar</button>
+                <button onClick={abrePreguntaBorrado}>Borrar</button>
               </div>
             </div>
             <hr /> {/* Línea para separar visualmente los ejercicios */}
@@ -113,6 +157,96 @@ export function Ejercicios() {
            </div>
           </div> 
       )}
-    </div>
-  );
+  
+
+  {mostrarInfo && (
+    <div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', background: 'rgba(0, 0, 0, 0.5)', zIndex: '1' }}>
+    <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: 'white', padding: '20px', borderRadius: '8px', width: '300px' }}>
+      
+      <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+      <h4>Info ejercicio</h4>
+      <button onClick={cerrarInfo}>Volver</button>
+      {/* Cierra la ventana y te da mensaje positivo pero no añade nada aún!!!! */}
+      </div>
+      </div>
+     </div> 
+ )}
+
+{modificarInfo && (
+        <div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', background: 'rgba(0, 0, 0, 0.5)', zIndex: '1' }}>
+          <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: 'white', padding: '20px', borderRadius: '8px', width: '300px' }}>
+            
+            <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+            <h3>Modificar Ejercicio</h3>
+            <button onClick={modifCerrarInfo}>Cancelar</button>
+            </div>
+            <p>Nombre: <input type="text" style = {{marginLeft:'10px'}}/></p>
+            
+            <p>Tipo: <input type="text" style = {{marginLeft:'10px'}}/></p>
+           {/**  Esto es para ponerlo en modo seleccion, pero son valores fijados
+            <p style={{marginRight:'100px'}}>Link:  
+            
+              <select id="Link" name="Link">
+              <option value="Cardio">Cardio</option>
+              <option value="Fuerza">Fuerza</option>
+              <option value="Flexibilidad">Flexibilidad</option>
+              </select>
+            </p>
+            **/}
+
+            <p>Link: <input type="text" style = {{marginLeft:'10px'}}/></p>
+            <p>Descripcion: <input type="text" id="descripcion" style = {{marginLeft:'10px'}}/></p>
+            
+            <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+           
+            <label><input type= "checkbox" value = "Privado" /> Privado </label>
+            
+            <button onClick={abrirVentanaConfirmacionModif}>Confirmar</button>
+            {/* Este debería mostrar una ventana de ejercicio añadido y mostrarlo */}
+            </div>
+          </div>
+        </div>
+      )}
+
+        {mostrarVentanaConfirmacionModif && (
+         <div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', background: 'rgba(0, 0, 0, 0.5)', zIndex: '1' }}>
+         <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: 'white', padding: '20px', borderRadius: '8px', width: '300px' }}>
+           
+           <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+           <h4>El ejercicio ha sido modificado</h4>
+           <button onClick={cerrarVentanaConfirmacionModif}>Ok</button>
+           {/* Cierra la ventana y te da mensaje positivo pero no añade nada aún!!!! */}
+           </div>
+           </div>
+          </div> 
+      )}
+
+          {preguntaBorrado && (
+              <div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', background: 'rgba(0, 0, 0, 0.5)', zIndex: '1' }}>
+              <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: 'white', padding: '20px', borderRadius: '8px', width: '300px' }}>
+                
+                <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+                <h4>¿Está seguro de borrar este ejercicio?</h4>
+                <button onClick={abrirVentanaConfirmacionBorrado}>Si</button>
+                <button onClick={cierraPreguntaBorrado}>No</button>
+                {/* Cierra la ventana y te da mensaje positivo pero no borra nada aún!!!! */}
+                </div>
+                </div>
+              </div> 
+          )}
+
+        {mostrarVentanaConfirmacionBorrado && (
+                <div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', background: 'rgba(0, 0, 0, 0.5)', zIndex: '1' }}>
+                <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: 'white', padding: '20px', borderRadius: '8px', width: '300px' }}>
+                  
+                  <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+                  <h4>El ejercicio ha sido eliminado correctamente</h4>
+                  <button onClick={cerrarVentanaConfirmacionBorrado}>Ok</button>
+                  </div>
+                  </div>
+                  </div> 
+              )}
+
+</div>
+);
 }
