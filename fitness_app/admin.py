eeb54dc8c6bf31a5ee976f7d7b2d546d5325afbd
firehellrain <firehellrain@gmail.com>
2023-11-22 12:51:37 +0100

    Añadido modelos a panel admin
    - Se han añadido los modelos de la base de datos al panel de administración del backend para poder visualizar las entradas

--- fitness_app/admin.py
+++ fitness_app/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class ClientAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('User', {'fields': ['user']}),
+        ('Trainer', {'fields': ['trainer']}),
+    ]
+
+
+class TrainerAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('User', {'fields': ['user']}),
+        ('Category', {'fields': ['category']}),
+    ]
+
+
+class RoutineAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Objective', {'fields': ['objective']}),
+        ('Client', {'fields': ['client']}),
+        ('Trainer', {'fields': ['trainer']}),
+    ]
+
+
+class TrainingSessionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Repetitions', {'fields': ['rep']}),
+        ('Sets', {'fields': ['sets']}),
+        ('Exercise', {'fields': ['exercise']}),
+    ]
+
+
+class RatingInLine(admin.TabularInline):
+    model = Rating
+    extra = 1
+
+
+class ExerciseAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Name', {'fields': ['name']}),
+        ('Description', {'fields': ['desc']}),
+        ('Type', {'fields': ['type']}),
+        ('Video URL', {'fields': ['videoURL']}),
+        ('Is Private?', {'fields': ['isPrivate']}),
+        ('Trainer', {'fields': ['trainer']}),
+    ]
+    inlines = [RatingInLine]
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Trainer, TrainerAdmin)
+admin.site.register(Routine, RoutineAdmin)
+admin.site.register(TrainingSession, TrainingSessionAdmin)
+admin.site.register(Exercise, ExerciseAdmin)
