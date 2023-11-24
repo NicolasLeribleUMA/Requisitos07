7f136b108859e5d4e55f72bdf406f475e2a67292
firehellrain <firehellrain@gmail.com>
2023-11-24 19:26:45 +0100

    Modelo Citas y añadido campos
    - Creado el modelo 'Appointment' en la base de datos para gestionar las citas de los clientes con los entrenadores
    - Añadido varios campos de información al cliente
    - Añadido el campo 'comment' en 'Rating' para poder añadir un comentario a la valoración de los ejercicios

--- fitness_app/models.py
+++ fitness_app/models.py
@@ -13,6 +13,10 @@ class Trainer(models.Model):
 
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    weight = models.IntegerField
+    imc = models.IntegerField
+    measureDate = models.DateField
+    objective = models.CharField(max_length=200)
     trainer = models.ForeignKey(Trainer, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
@@ -60,9 +64,19 @@ class Routine(models.Model):
 
 
 class Rating(models.Model):
-    rating = models.IntegerField
+    rating = models.BooleanField
+    comment = models.CharField(max_length=300)
     client = models.OneToOneField(Client, on_delete=models.CASCADE)
     exercise = models.OneToOneField(Exercise, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Valoración {self.exercise.name} de {self.client}: {self.rating}"
+
+
+class Appointment(models.Model):
+    date = models.DateField
+    client = models.OneToOneField(Client, on_delete=models.CASCADE)
+    trainer = models.OneToOneField(Trainer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Cita con {self.client.user.username} el día {self.date}"
