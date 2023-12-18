9b91dfd99778e533ca22354eb7146d82aa47388a
BrotherZ11 <dzarzavillaborrego@uma.es>
2023-12-18 23:05:07 +0100

    Info cliente

--- client/src/pages/trainer/VerEntrenamiento.jsx
+++ client/src/pages/trainer/VerEntrenamiento.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navigation";
 import { Link, useParams } from "react-router-dom";
 import { getCliente } from "../../api/Clientes";
+import "./VerEntrenamiento.css"; // Importa tu archivo CSS para estilos
 
 const VerEntrenamiento = () => {
   const { clienteId } = useParams();
@@ -32,16 +33,20 @@ const VerEntrenamiento = () => {
       <h2>Entrenamiento del Cliente</h2>
 
       <div className="entrenamiento-container">
-        {/* Información del Cliente */}
-        <div className="cliente-info-container">
-          <h3>Información del Cliente</h3>
-          <p>Nombre: {clienteId.user}</p>
-          {/* Otros detalles del cliente */}
-        </div>
-
         {/* Valoración y Progreso */}
         <div className="valoracion-progreso-container">
           <h3>Valoración y Progreso</h3>
+        </div>
+
+        {/* Información del Cliente */}
+        <div className="cliente-info-container">
+          <h3>Información del Cliente</h3>
+          <p>Nombre: {cliente.user}</p>
+          <p>Peso: {cliente.weight}</p>
+          <p>IMC: {cliente.imc}</p>
+          <p>Objetivo: {cliente.objective}</p>
+          <p>Modificacion: {cliente.measureDate}</p>
+          {/* Otros detalles del cliente */}
         </div>
       </div>
 
